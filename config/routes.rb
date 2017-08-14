9a8d568ad2c030f23fb558f2b103ebefcfdf3fca
JoelLindow <joellindow@gmail.com>
2017-08-14 11:57:55 -0600

    new language form and functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   resources :coders, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  resources :languages, only: [:index]
+  resources :languages, only: [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
